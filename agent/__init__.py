92e26654619faabc809fada182547f8cbf4bf865
comyn <me@xueming.li>
2016-11-19 19:53:10 +0800

    recommit

--- agent/__init__.py
+++ agent/__init__.py
@@ -3,42 +3,56 @@ import struct
 import socket
 import logging
 import datetime
+import threading
 from .command import Command
 
 __version__ = '0.0.1'
 
 
+def encode(data):
+    buf = json.dumps(data).encode()
+    length = len(buf)
+    return struct.pack('<l{}s'.format(length), length, buf)
+
+
 class Agent:
     def __init__(self, master):
         self.master = master
-        self.task = {}
+        self.tasks = {}
         self.so = None
+        self.event = threading.Event()
 
     def connect(self):
         self.so = socket.socket()
         self.so.connect(self.master)
 
-    def encode(self, data):
-        buf = json.dumps(data).encode()
-        length = len(buf)
-        return struct.pack('<l{}s'.format(length), length, buf)
-
     def heartbeat(self):
         data = {
             'version': __version__,
             'timestamp': datetime.datetime.now().timestamp(),
-            'task': self.task.get('current')
+            'task': self.tasks.get('current')
         }
         try:
-            self.so.send(self.encode(data))
+            self.so.send(encode(data))
             if data.get('task') is None:
                 buf = self.so.recv(4)
                 length, _ = struct.unpack('<l', buf)
                 buf = self.so.recv(length)
                 data, _ = struct.unpack('<{}s'.format(length), buf)
-                data = json.loads(data.decode())
-                cmd = Command(data)
-                cmd.run()
+                task = json.loads(data.decode())
+                cmd = Command(task)
+                future = cmd.run()
+                if future is not None:
+                    future.add_done_callback(lambda: self.tasks.pop('current', None))
+                self.tasks['current'] = task
         except Exception as e:
             logging.error('send heartbeat error: {}'.format(e))
             self.connect()
+
+    def start(self):
+        while not self.event.is_set():
+            self.heartbeat()
+            self.event.wait(1)
+
+    def shutdown(self):
+        self.event.set()
--- agent/command.py
+++ agent/command.py
@@ -1,18 +1,27 @@
 from subprocess import Popen, PIPE, STDOUT
+from concurrent.futures import ThreadPoolExecutor
+from threading import Event
 
 
 class Command:
+    executor = ThreadPoolExecutor(max_workers=1)
+    event = Event()
+
     def __init__(self, task):
-        self.script = task['script']
-        self.timeout = task.get('timeout', 30)
         self.task = task
 
-    def run(self):
-        with Popen(['/bin/bash', '-l', '-c', self.script],
+    def __run(self):
+        self.event.set()
+        with Popen(['/bin/bash', '-l', '-c', self.task['script']],
                    stdout=PIPE,
                    stderr=STDOUT,
                    start_new_session=True) as proc:
-            code = proc.wait(self.timeout)
+            code = proc.wait(self.task.get('timeout', 30))
             out, _ = proc.communicate()
             self.task['code'] = code
             self.task['output'] = out
+        self.event.clear()
+
+    def run(self):
+        if not self.event.is_set():
+            return self.executor.submit(self.__run)
